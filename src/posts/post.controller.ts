78a799e606cce20ef8f41f823f9ce1c71903444a
John Alves <johnvictorio3@gmail.com>
2021-11-08 22:18:51 -0300

    Feat(PostController): show post by id

--- src/posts/post.controller.ts
+++ src/posts/post.controller.ts
@@ -1,8 +1,10 @@
 import {
   Body,
   Get,
+  NotFoundException,
+  ParseIntPipe,
 } from '@nestjs/common';
-import { Controller, Post as PostHttp } from '@nestjs/common';
+import { Controller, Param, Post as PostHttp } from '@nestjs/common';
 import { Post } from './post';
 import { PostDTO } from './post.dto';
 import { PostService } from './post.service';
@@ -21,5 +23,18 @@ export class PostController {
   @PostHttp('/')
   public create(@Body() body: PostDTO): Promise<Post> {
     return this.service.create(new Post(body));
+  }
+
+  @Get('/:postId')
+  public async show(
+    @Param('postId', ParseIntPipe) postId: number,
+  ): Promise<Post> {
+    const post = await this.service.findOneById(postId);
+
+    if (!post) {
+      throw new NotFoundException(null, `Post with id ${postId} not exists`);
+    }
+
+    return post
   }
 }
