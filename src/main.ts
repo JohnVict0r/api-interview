a57affacb7483d40dbc85e2a3c53126fcd34cfaf
John Alves <johnvictorio3@gmail.com>
2021-11-12 11:06:33 -0300

    add Procfile for deploy in heroku

--- src/main.ts
+++ src/main.ts
@@ -29,6 +29,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3333);
+  await app.listen(process.env.PORT || 3333);
 }
 bootstrap();
